16fbb251c07b25bf930cfd5498be214442528720
PetrosMaltos <peterimovpeter@gmail.com>
2024-12-30 19:15:43 +0500

    first commit

---
+++ src/components/MainScreen.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { FaWallet } from 'react-icons/fa';
+import './MainScreen.css';
+import logo from './logo.png'; // Импортируем логотип
+
+const MainScreen = () => {
+  const [connected, setConnected] = useState(false);
+  const [tokens, setTokens] = useState(1000); // Пример баланса токенов
+
+  const handleConnectWallet = () => {
+    setConnected(true);
+  };
+
+  return (
+    <div className="main-screen">
+      {/* Кнопка подключения кошелька */}
+      <div className="wallet-connect">
+        {connected ? (
+          <p>Telegram Wallet Connected</p>
+        ) : (
+          <button onClick={handleConnectWallet} className="button-33" role="button">
+            Connect <FaWallet style={{ marginLeft: '8px', marginRight: '0px' }} />
+          </button>
+        )}
+      </div>
+
+      {/* Логотип */}
+      <div className="logo">
+        <img src={logo} alt="Mintero Logo" />
+      </div>
+
+      {/* Баланс токенов */}
+      <div className="token-balance">
+        <h2>{tokens} Mintero</h2>
+      </div>
+
+      <div className="feature">
+        <p>The app's main feature will be here soon!</p>
+      </div>
+    </div>
+  );
+};
+
+export default MainScreen;
